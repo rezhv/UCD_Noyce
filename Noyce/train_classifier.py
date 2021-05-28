1e52a6d71786567a9bb52a259c611934b706865e
rezhv <al.havaei@icloud.com>
2021-05-27 21:07:02 -0700

    Update

--- Noyce/train_classifier.py
+++ Noyce/train_classifier.py
@@ -13,8 +13,10 @@ from trainer import Custome_Trainer
 
 
 def prepare_trainer(args):
+
   device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-  if (device == "cpu"):
+
+  if (not torch.cuda.is_available()):
     print("WARNING: TRAINING ON CPU. THIS WILL BE SLOW.")
   if (not args.verbose):
     transformers.logging.set_verbosity_error()
