4aef2e0b1d3ebea70971fbd516ed3d2e423deb83
rezhv <al.havaei@icloud.com>
2021-05-30 00:30:47 -0700

    Disagreement DS Support

--- Noyce/trainer.py
+++ Noyce/trainer.py
@@ -15,9 +15,9 @@ class Custome_Trainer(Trainer):
         labels = inputs['labels'].to(self.args.device)
         outputs = self.model(input_ids)
         logits = outputs.logits
+        print(logits , labels)
         loss_fct = torch.nn.CrossEntropyLoss()
         loss = loss_fct(logits, labels)
-        print(loss)
         return (loss, {"logits": outputs.logits}) if return_outputs else loss
 
 
