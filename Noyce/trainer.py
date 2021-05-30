9939678ed88f8b6ab761c3bf3e66938f60ea5203
rezhv <al.havaei@icloud.com>
2021-05-30 00:36:52 -0700

    Num Labels Automation

--- Noyce/trainer.py
+++ Noyce/trainer.py
@@ -15,7 +15,6 @@ class Custome_Trainer(Trainer):
         labels = inputs['labels'].to(self.args.device)
         outputs = self.model(input_ids)
         logits = outputs.logits
-        print(logits , labels)
         loss_fct = torch.nn.CrossEntropyLoss()
         loss = loss_fct(logits, labels)
         return (loss, {"logits": outputs.logits}) if return_outputs else loss
