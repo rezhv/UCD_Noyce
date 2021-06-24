ae29bc254c0f8a4e23e9b87aa2f600fb14c18151
rezhv <al.havaei@icloud.com>
2021-06-23 22:18:56 -0700

    confidence

--- Noyce/callbacks.py
+++ Noyce/callbacks.py
@@ -33,6 +33,6 @@ class export_predictions_callback(TrainerCallback):
                 text = text + tokenizer.batch_decode(batch['input_ids'],skip_special_tokens=True)
                 predictions = predictions + torch.argmax(outputs, axis=1).cpu().numpy().tolist()
                 labels = labels + batch['labels'].cpu().numpy().tolist()
-                confidence = outputs.cpu().numpy().tolist()
+                confidence = confidence + outputs.cpu().numpy().tolist()
 
         self.export_predictions(text,labels,predictions, confidence)
